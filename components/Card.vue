73c672dbf9babf6dc97c8a0f00c8ec50a339c9fb
Rafael Dos Santos <rafasennin@hotmail.com>
2024-06-19 17:47:41 -0300

    update

--- components/Card.vue
+++ components/Card.vue
@@ -4,7 +4,9 @@
       <v-text-field v-model="taskAuthor" label="Autor"></v-text-field>
       <v-text-field v-model="taskName" label="Título da tarefa"></v-text-field>
       <v-text-field v-model="taskDate" label="Data de criação" readonly></v-text-field>
-      <v-textarea v-model="taskContent" label="Conteúdo da Tarefa"></v-textarea>
+      <v-date-input v-model="taskReminderDate" label="Data do lembrete"></v-date-input>
+      <v-text-field v-model="taskReminderHour" label="Hora do lembrete" type="time"></v-text-field>
+      <v-textarea v-model="taskContent" label="Descreva sua tarefa"></v-textarea>
       <v-row>
         <v-col cols="12" class="d-flex justify-center">
           <v-btn color="primary" type="submit">Adicionar</v-btn>
@@ -24,6 +26,8 @@
           <th class="text-h5">Tarefa</th>
           <th class="text-h5">Data</th>
           <th class="text-h5">Descrição</th>
+          <th class="text-h5">Data de aviso</th>
+          <th class="text-h5">Hora de aviso</th>
         </tr>
       </thead>
       <tbody>
@@ -32,9 +36,16 @@
           <td>{{ task.title }}</td>
           <td>{{ task.date }}</td>
           <td class="word-wrap-break">{{ task.text }}</td>
+          <td>{{ task.reminderDate }}</td>
+          <td>{{ task.reminderHour }}</td>
           <td>
             <v-btn v-if="loginStatus" @click="deleteTask(task._id)" class="text-decoration-none bg-red-darken-4">
               <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </td>
+          <td>
+            <v-btn v-if="loginStatus" @click="openEditModal(task)" class="text-decoration-none bg-blue-darken-2">
+              <v-icon>mdi-pencil</v-icon>
             </v-btn>
           </td>
         </tr>
@@ -44,17 +55,44 @@
       {{ currentDate }}
       <v-progress-circular v-if="loadingFlag" color="primary" indeterminate :size="74" :width="8"></v-progress-circular>
     </tr>
+
+    <!-- Modal for editing the task -->
+    <v-dialog v-model="editModal" max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Editar Tarefa</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="editForm">
+            <v-text-field v-model="editTaskAuthor" label="Autor"></v-text-field>
+            <v-text-field v-model="editTaskName" label="Título da tarefa"></v-text-field>
+            <v-date-input v-model="editTaskReminderDate" label="Data do lembrete"></v-date-input>
+            <v-text-field v-model="editTaskReminderHour" label="Hora do lembrete" type="time"></v-text-field>
+            <v-textarea v-model="editTaskContent" label="Conteúdo da Tarefa"></v-textarea>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeEditModal">Cancelar</v-btn>
+          <v-btn color="blue darken-1" text @click="confirmEditTask">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import axios from 'axios';
+import { VDateInput } from 'vuetify/labs/VDateInput';
+import { VTimePicker } from 'vuetify/labs/VTimePicker'
+
 
 const router = useRouter();
 const isLogged = useState('isLogged', () => false);
 
 const loginStatus = isLogged.value;
+
 
 const getCurrentDate = () => {
   const date = new Date();
@@ -66,12 +104,21 @@ const getCurrentDate = () => {
 
 const taskAuthor = useState('taskAuthor', () => '');
 const taskName = useState('taskName', () => '');
-const taskDate = useState('taskDate', () => getCurrentDate()); // Chamando getCurrentDate aqui
+const taskDate = useState('taskDate', () => getCurrentDate());
 const taskContent = useState('taskContent', () => '');
+const taskReminderDate = useState('taskReminderDate', () => '');
+const taskReminderHour = useState('taskReminderHour', () => '');
 const tasks = useState('tasks', () => []);
 const isAdding = useState('isAdding', () => false);
 const isFetching = useState('isFetching', () => false);
 
+const editModal = ref(false);
+const editTaskId = ref('');
+const editTaskAuthor = ref('');
+const editTaskName = ref('');
+const editTaskContent = ref('');
+const editTaskReminderDate = ref('');
+const editTaskReminderHour = ref('');
 
 const getTasks = async () => {
   try {
@@ -93,14 +140,17 @@ const addTask = async () => {
         author: taskAuthor.value,
         title: taskName.value,
         date: getCurrentDate(),
-        text: taskContent.value
+        text: taskContent.value,
+        reminderDate: taskReminderDate.value,
+        reminderHour: taskReminderHour.value
       };
       await axios.post("https://lista-de-tarefas-back-end-lake.vercel.app/tasks", task);
       getTasks();
-      // Limpar campos do formulário
       taskAuthor.value = '';
       taskName.value = '';
       taskContent.value = '';
+      taskReminderDate.value = '';
+      taskReminderHour.value = '';
     } catch (error) {
       console.error("Erro ao enviar tarefa:", error);
     } finally {
@@ -111,16 +161,50 @@ const addTask = async () => {
   }
 };
 
+
 const deleteTask = async (taskId: string) => {
   try {
     await axios.delete(`https://lista-de-tarefas-back-end-lake.vercel.app/tasks/${taskId}`);
-    // Atualiza a lista de tasks após a exclusão
     getTasks();
   } catch (error) {
     console.error("Erro ao excluir tarefa:", error);
   }
 };
 
-// Chamar getTasks ao montar o componente
+const openEditModal = (task: any) => {
+  editTaskId.value = task._id;
+  editTaskAuthor.value = task.author;
+  editTaskName.value = task.title;
+  editTaskContent.value = task.text;
+  editTaskReminderDate.value = task.reminderDate;
+  editTaskReminderHour.value = task.reminderHour;
+  editModal.value = true;
+};
+
+
+const closeEditModal = () => {
+  editModal.value = false;
+};
+
+const confirmEditTask = async () => {
+  try {
+    const updatedTask = {
+      author: editTaskAuthor.value,
+      title: editTaskName.value,
+      date: getCurrentDate(),
+      text: editTaskContent.value,
+      reminderDate: editTaskReminderDate.value,
+      reminderHour: editTaskReminderHour.value
+    };
+
+    await axios.put(`https://lista-de-tarefas-back-end-lake.vercel.app/tasks/${editTaskId.value}`, updatedTask);
+    closeEditModal();
+    getTasks();
+  } catch (error) {
+    console.error("Erro ao editar a tarefa:", error);
+  }
+};
+
+
 getTasks();
 </script>
