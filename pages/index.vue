f3b38661f1ac9ff417053482250fdb9006dd1cb1
Rafael Dos Santos <rafasennin@hotmail.com>
2024-06-25 14:53:43 -0300

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
       <!-- Banner Explicativo -->
       <v-row class="d-flex justify-center my-16 body-row" v-if="!isLoggedStatus">
         <v-col cols="12" class="text-center">
-          <div ref="banner" class="banner">
+          <div ref="banner">
             <h1 class="display-1 font-weight-bold text-red mb-4">Lista de Tarefas</h1>
-            <p class="text-h4 text-red-lighten-2 mb-2">Bem-vindo à aplicação de gerenciamento de tarefas!</p>
+            <p class="text-h5 text-red-lighten-2 mb-2">Bem-vindo à aplicação de gerenciamento de tarefas!</p>
             <p class="text-h5 text-red-lighten-2">
               Para começar, faça login se já estiver cadastrado ou cadastre-se para acessar sua lista de tarefas.
             </p>
@@ -16,7 +16,7 @@
       <!-- Validação de Login -->
       <v-row v-if="isLoggedStatus" class="d-flex justify-center mt-16">
         <v-col cols="12" class="text-center">
-          <div ref="loggedStatus" class="logged-status">
+          <div ref="loggedStatus">
             <h2 class="headline text-vivid">Logado como {{ getUserName }}</h2>
             <LoginComponent />
           </div>
@@ -24,7 +24,7 @@
       </v-row>
       <v-row v-if="isLoggedStatus" class="d-flex justify-center mt-4">
         <v-col cols="12" class="text-center">
-          <div ref="card" class="card">
+          <div ref="card">
             <Card />
           </div>
         </v-col>
@@ -33,7 +33,7 @@
       <!-- Mensagem para Usuário Não Logado -->
       <v-row v-else class="d-flex justify-center mt-4">
         <v-col cols="12" class="text-center">
-          <div ref="loginComponent" class="login-component">
+          <div ref="loginComponent">
             <LoginComponent />
           </div>
         </v-col>
@@ -103,19 +103,6 @@
   .display-1 {
       font-size: 3rem;
       font-family: 'Lobster', cursive;
-  }
-    
-  .banner, .logged-status, .card, .login-component {
-      animation: fadeIn 1.5s ease-in-out;
-  }
-  
-  @keyframes fadeIn {
-      from {
-          opacity: 0;
-      }
-      to {
-          opacity: 1;
-      }
   }
   </style>
   
